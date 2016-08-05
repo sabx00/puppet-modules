1e9b8f071d5595b5993317ce0115c40ff41e02a6
santiagoabx@hotmail.com <santiagoabx@hotmail.com>
2016-08-05 15:28:00 +0200

    agregue algo al pp

--- test1/manifests/init.pp
+++ test1/manifests/init.pp
@@ -44,5 +44,9 @@
 #
 class test1 {
 
+# execute 'apt-get update'
+exec { 'apt-update':                    # exec resource named 'apt-update'
+  command => '/usr/bin/apt-get update'  # command this resource will run
+}
 
 }
